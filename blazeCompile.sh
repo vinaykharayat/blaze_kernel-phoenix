a440a4bd1f577d59fdaf381376894a61015d2761
vinaykharayat <vinaykharayat@gmail.com>
2021-01-16 18:25:18 +0530

    removed unnecessary files

--- blazeCompile.sh
+++ blazeCompile.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-make O=out clean
-make O=out mrproper
-make O=out ARCH=arm64 phoenix_defconfig
-export ARCH=arm64 && export SUBARCH=arm64
-export CROSS_COMPILE=$(pwd)/gcc/bin/aarch64-linux-android-
-PATH="$(pwd)/tc/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=aarch64-linux-android- \
-                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
-
+echo 1 for cleaning, 2 for clean and build
+read mInput
+if [ $mInput -eq "2" ]
+	then
+		make O=out clean
+		make O=out mrproper
+		make O=out ARCH=arm64 phoenix_defconfig
+		export ARCH=arm64 && export SUBARCH=arm64
+		export CROSS_COMPILE=$(pwd)/gcc/bin/aarch64-linux-android-
+		PATH="$(pwd)/tc/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:${PATH}" \
+		make -j$(nproc --all) O=out \
+                      		ARCH=arm64 \
+                      		CC=clang \
+                      		CLANG_TRIPLE=aarch64-linux-gnu- \
+                      		CROSS_COMPILE=aarch64-linux-android- \
+                      		CROSS_COMPILE_ARM32=arm-linux-androideabi-
+	else
+        make O=out clean
+		make O=out mrproper
+fi
